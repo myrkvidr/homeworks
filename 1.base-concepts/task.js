9a106aeab0a7930bacc8b9efddaca28d3bcbc7b3
Inna Savchenko <sonmie@yandex.ru>
2022-08-11 01:20:24 +0500

    Homework 1 & 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,54 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let x1;
+  let x2;
+  let discr = (b ** 2) - (4 * a * c);
+  if (discr < 0) {
+    return arr;
+  } else if (discr === 0) {
+    x1 = -b / (2 * a);
+    arr.push(x1);
+    return arr;
+  } else {
+    x1 = (-b + Math.sqrt(discr)) / (2 * a);
+    x2 = (-b - Math.sqrt(discr)) / (2 * a);
+    arr.push(x1, x2);
+    return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  let percentNum = +percent / 100;
+  let contributionNum = +contribution;
+  let amountNum = +amount;
 
-  // код для задачи №2 писать здесь
+  if (Number.isNaN(percentNum)) {
+    return `Параметр \"Процентная ставка\" содержит неправильное значение \"${percent}\"`;
+  } 
+  if (Number.isNaN(contributionNum)) {
+    return `Параметр \"Начальный взнос\" содержит неправильное значение \"${contribution}\"`;
+  }
+  if (Number.isNaN(amountNum)) {
+    return `Параметр \"Общая стоимость\" содержит неправильное значение \"${amount}\"`;
+  }
 
+  let creditBody = amountNum - contributionNum; 
+
+  let startDate = new Date();
+  let startYear = startDate.getFullYear();
+  let endYear = date.getFullYear();
+  let months = (endYear - startYear) * 12;
+  
+  let monthlyPercent = percentNum / 12;
+  let monthlyPayment = creditBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** months) - 1)));
+
+  
+  totalAmount = monthlyPayment * months;
+  totalAmount = Math.round(totalAmount * 100) / 100;
+
+  console.log(totalAmount);
   return totalAmount;
 }
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,58 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg;
 
-  // Ваш код
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
 
+  avg = +(sum / arr.length).toFixed(2);
+  
   return { min: min, max: max, avg: avg };
 }
 
+
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = -Infinity;
+  for (x = 0; x < arrOfArr.length; x++) {
+    let a = arrOfArr[x];
+    if (func(a) > max) {
+      max = func(a);
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+  for (y = 0; y < arr.length; y++) {
+    if (arr[y] < min) {
+      min = arr[y];
+    }
+    if (arr[y] > max) {
+      max = arr[y];
+    }
+  }
+  let dist = Math.abs(max - min);
+  return dist;
 }
