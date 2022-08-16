7a87989ec9b2786572dc4cb7315731b8c7fb629a
Inna Savchenko <sonmie@yandex.ru>
2022-08-16 22:23:52 +0500

    Homework 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result; 
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    result = (arr1.every((val, index) => val === arr2[index]));
+  }
+  return result;
 }
+
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((num) => num >= 0).filter((num) => num % 3 === 0).map((num) => num * 10);
   return resultArr; // array
 }
+
+
+
