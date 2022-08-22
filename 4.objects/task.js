fc2e5003d132b6b51e667e474c910265255ea1d0
Inna Savchenko <sonmie@yandex.ru>
2022-08-22 23:55:19 +0500

    Homework 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,49 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+// const student1 = new Student("Hermione", "female", 17);
+// const student2 = new Student("Luna", "female", 15);
+// const student3 = new Student("Neville", "male", 17);
+
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  let marks = [mark];
+  if(this.marks === undefined) {
+    this.marks = marks;
+  } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function(...mark) {
+  let marks = [];
+  if(this.marks === undefined) {
+    this.marks = marks;
+    this.marks.push(...mark);
+  } else {
+      this.marks.push(...mark);
+    }
+}
+
+Student.prototype.getAverage = function() {
+  let markSum = 0;
+  let count = 0;
+  for (let i = 0; i < this.marks.length; i++) {
+    count += 1;
+    markSum += this.marks[i];
+  }
+  let average = markSum / count;
+  return average;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
